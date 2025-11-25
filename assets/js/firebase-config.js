10676bb64da6e7569f2822eba205f4e7e8583211
Fedik2005 <fedya.petrov.0@mail.ru>
2025-11-25 17:11:17 +0400

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQoxE0cF0RuuxeZlJk-f8tS-yVSoK8soc",
+  authDomain: "daas-platform-378af.firebaseapp.com",
+  projectId: "daas-platform-378af",
+  storageBucket: "daas-platform-378af.firebasestorage.app",
+  messagingSenderId: "417519877430",
+  appId: "1:417519877430:web:ccd6a9a8e06f97c9d28797",
+  measurementId: "G-7Z63VJ0X04"
+};
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
